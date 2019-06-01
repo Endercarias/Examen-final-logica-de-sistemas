4baea36d3386d86d442615c93549e581c49b0f8b
Endercarias <50668460+Endercarias@users.noreply.github.com>
2019-06-01 09:04:19 -0600

    Examen de logica

---
+++ Index.py
@@ -0,0 +1,23 @@
+
+print ("valor1.")
+print ("valor2.")
+print ("valor3.")
+
+
+# ingresar los tres valores
+    valor1 = input("Ingrese valor 1:  ")
+    valor2 = input("Ingrese valor 2:  ")
+    valor3 = input("Ingrese valor 3:  ")
+# primer condicion con la que funcionaria el primer boton pero sin la interfaz
+    if valor1 > valor3:
+        multiplicacion = int(valor1) * int(valor2) * int(valor3)
+        print("valor1*valor2*valor3: Resultado: {}".format(multiplicacion)
+#segunda condicion si son iguales 
+    if valor1 == valor2 == valor3:
+        suma = int(valor1) * int(valor2) * int(valor3)
+        print("valor1+valor2+valor3 Resultado: {}".format(suma)
+# tercera condicion si el valor 2 es igual a 0
+    if valor2 == 0:
+        
+    
+    
